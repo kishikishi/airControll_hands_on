2571d8e1ee6225b11b212566b71a91eb2dc509c8
Nobuaki Kishigami <30922647+kishikishi@users.noreply.github.com>
2020-07-26 20:18:28 +0900

    add Commnents

--- python_script/getTimeline.py
+++ python_script/getTimeline.py
@@ -1,8 +1,6 @@
 import tweepy
 import os 
 import datetime
-from bluepy.btle import Peripheral, DefaultDelegate, Scanner, BTLEException, UUID
-import bluepy.btle
 import sys
 import struct
 import argparse
@@ -11,6 +9,7 @@ import time
 import csv
 
 ''' twitter settings '''
+''' 発行されたコードを記入 '''
 CK="xxxxx"
 CS="xxxxx"
 AT="xxxxx"
@@ -21,56 +20,13 @@ auth.set_access_token(AT, AS)
 
 api = tweepy.API(auth)
 
-''' about ble '''
-devs = {
-    'omron': {'companyID': 'd502'},
-    'esp32': {'companyID': 'ffff'}
-}
-target = 'esp32'
-
-Debugging = False
-Verbose = True
-
-def dataInput(*args):
-    list = ['item1', 'item2']
-    with open('data.csv', 'w') as file:
-        writer = csv.writer(file, lineterminator='\n')
-        for data in list:
-	        writer.writerow(data)
-    with open('data.csv', 'w') as file:
-        writer = csv.writer(file, lineterminator='\n')
-        for data in args:
-            writer.writerow(data)
-
-def send2ambient(dataRow):
-    if companyID == 'ffff':
-        (temp, humid, press) = struct.unpack('<hhh', bytes.fromhex(dataRow))
-        dataInput(temp / 100, humid / 100, press / 10)
-
-class ScanDelegate(DefaultDelegate):
-    def __init__(self):
-        DefaultDelegate.__init__(self)
-        self.lastseq = None
-        self.lasttime = datetime.fromtimestamp(0)
-
-    def handleDiscovery(self, dev, isNewDev, isNewData):
-        if isNewDev or isNewData:
-            for (adtype, desc, value) in dev.getScanData():
-                # print(adtype, desc, value)
-                if target in ('omron', 'esp32'):
-                    if desc == 'Manufacturer' and value[0:4] == devs[target]['companyID']:
-                        delta = datetime.now() - self.lasttime
-                        if value[4:6] != self.lastseq and delta.total_seconds() > 11:
-                            self.lastseq = value[4:6]
-                            self.lasttime = datetime.now()
-                            send2ambient(value[6:])
-                            
-def main():
-    for status in api.home_timeline():
-        txt = status.text
+'''ツイート内容に応じて信号送信 '''                            
+def watchTweet():
+    for status in api.home_timeline(): #status:タイムラインの情報
+        txt = status.text　#statusのうち、tweetを取得
         if  txt =='cool':
-            os.system('irsend SEND_ONCE aircon coolstart')
-            api.destroy_status(status.id)
+            os.system('irsend SEND_ONCE aircon coolstart') #shell commandを実行
+            api.destroy_status(status.id) #tweetを削除
             scanner = Scanner().withDelegate(ScanDelegate())
             scanner.scan(30.0)
             break
@@ -85,6 +41,9 @@ def main():
 	        api.destroy_status(status.id)
 	        break
 
+		
+'''スケジュール予約'''
+def dailyEvent
     weekday = datetime.date.today().weekday()
     dt_now = datetime.datetime.now()
     
@@ -94,5 +53,5 @@ def main():
             os.system('irsend SEND_ONCE aircon warmstart')
     '''
 if __name__ == "__main__":
-    main()
+    watchTweet()
  
